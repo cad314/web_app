668d7f275dc67fc5ab29f65ff25869cdeadba653
stephannielam <101894>
2017-05-09 15:01:49 -0700

    commit from tues

--- routes/profile.js
+++ routes/profile.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/student', function(req, res, next) {
-    res.render('student_profile.ejs', { title: 'CALENDAR APP' });
+    res.render('student.ejs', { title: 'CALENDAR APP' });
 });
 
 router.get('/professor', function(req, res, next) {
-    res.render('professor_profile.ejs', { title: 'CALENDAR APP' });
+    res.render('instructor.ejs', { title: 'CALENDAR APP' });
 });
 
 
