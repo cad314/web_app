b2e0cbc94397cf40a9f6caaeeda4a91c0bd687bb
Carlos Downie <carlos@bpwebdesign.com>
2017-05-10 13:40:03 -0700

    Merge branch 'SLAMBAM' of https://github.com/cad314/web_app into cdownie
    # Conflicts:
    #	views/calendar.ejs

--- routes/profile.js
+++ routes/profile.js
@@ -56,4 +56,17 @@ router.get('/professor/edit', function(req, res, next) {
     }
 });
 
+router.get('/submit', function (req, res, next) {
+    if(user.logged_in && user.isStudent == 0) {
+        debug.log(req.body);
+        user.updateProf(req.body.first,req.body.last,)
+    }
+    else if(user.logged_in) {
+
+    }
+    else{
+        res.redirect('../'); //Go back to home page
+    }
+});
+
 module.exports = router;
